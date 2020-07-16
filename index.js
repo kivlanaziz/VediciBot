18d72bf7bb7a935dd491f4555d1ab2fe8cfb8b00
Kivlan Aziz Al-Falaah <kivlanaziz@gmail.com>
2020-07-16 10:12:45 +0700

    change token and add worker

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const Discord = require('discord.js'); 
 const client = new Discord.Client();
-const token = GetToken();
 
-client.login(token);
+client.login(process.env.token);
 
 client.on('ready', () => {   
     console.log('VediciBot Reporting!'); 
@@ -13,27 +12,3 @@ client.on('message', (msg) => {
         msg.reply('Hi! :)');
     }
 });
-
-function GetToken(){
-    var fs = require("fs");
-    var contents = fs.readFileSync("credential.json");
-    if (contents != null && contents != "undefined")
-    {
-        var jsonContent = JSON.parse(contents);
-        if (jsonContent.hasOwnProperty('token'))
-        {
-            console.log('token : '+jsonContent.token);
-            return jsonContent.token;
-        }
-        else
-        {
-            console.log('JSON File does not have token key');
-            return null;
-        }
-    }
-    else
-    {
-        console.log('Cannot Read JSON File');
-        return null;
-    }
-}
