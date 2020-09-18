ad95f0ccc8e5b43357851f82e9dda60e3bcc1fd1
Kivlan Aziz Al-Falaah <kivlanaziz@gmail.com>
2020-09-18 10:59:58 +0700

    add await when getting the lyrics

--- module/lyrics.js
+++ module/lyrics.js
@@ -16,7 +16,7 @@ function getLyrics(title, message){
         geniusclient.songs.search(title,{limit: 1})
         .then(results => {
             const lyrics = results[0];
-            message.channel.send(`**${lyrics.artist.name} - ${lyrics.title}**\n<${lyrics.lyrics()}>`)
+            message.channel.send(`**${lyrics.artist.name} - ${lyrics.title}**\n<${await lyrics.lyrics()}>`)
         }).catch(err => message.reply(err));
     }
     catch(err){
