1fa6432cbbe855dbbc1d92eb4a06514c909807d5
Kivlan Aziz Al-Falaah <kivlanaziz@gmail.com>
2021-02-01 19:33:50 +0700

    Add Music Roles Categories

--- module/roles.js
+++ module/roles.js
@@ -50,9 +50,12 @@ function updateRoles(message){
     if (message.member.hasPermission("ADMINISTRATOR")){
         var GAMES_ROLES_ID = "805759803791376404";
         var MOVIES_ROLES_ID = "805762970604404736";
+        var MUSIC_ROLES_ID = "805776467333873665";
 
         message.guild.members.cache.forEach(member => {
+            console.log("Updating Roles of: " + member);
             member.roles.add(GAMES_ROLES_ID).catch(console.error);
+            member.roles.add(MOVIES_ROLES_ID).catch(console.error);
             member.roles.add(MOVIES_ROLES_ID).catch(console.error);
         });
     }
