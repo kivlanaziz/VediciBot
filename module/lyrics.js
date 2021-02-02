e009a7b64c0e13d0cfe30dc4d488c0e4cf4dd071
Kivlan Aziz Al-Falaah <kivlanaziz@gmail.com>
2021-02-02 14:22:57 +0700

    update code to blacklist case insensitive

--- module/lyrics.js
+++ module/lyrics.js
@@ -6,7 +6,8 @@ function cleanUpTitle(title){
     var newTitle = title.toString();
     var blacklist = ["Official Video", "OFFICIAL MUSIC VIDEO", "Music Video"];
     blacklist.forEach(item => {
-        newTitle = newTitle.replace(item,"");
+        var regexp = new RegExp(item,"gi");
+        newTitle = newTitle.replace(regexp,"");
     });
 
     return newTitle;
